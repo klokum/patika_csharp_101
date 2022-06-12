0c877df9532f3b88087a41b64358a0a198e0c260
Kaan Lokum <kaanlokum@gmail.com>
2022-06-12 18:16:13 +0300

    if-else-if

---
+++ if-else-if/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour; //O anki saati aldık.
+
+        if(time >=6 && time < 11)
+            Console.WriteLine("Günaydın!");
+        else if (time <= 18)
+            Console.WriteLine("İyi Günler!");
+        else
+            Console.WriteLine("iyi Geceler!");
+
+        //Ternary If
+        string sonuc = time <=18 ? "İyi Günler!" : "İyi Geceler!";
+        Console.WriteLine(sonuc);
+        
+        //İki ternary if'i birleştirirsek --> Yukarıdaki gibi
+        sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "İyi Günler!": "iyi Geceler!";
+        Console.WriteLine(sonuc);
+ 
+    }
+}
